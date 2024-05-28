ae66e05506a26f670c52adc41fae38b6f41c6647
GetGoh <agohca@gmail.com>
2024-05-28 13:28:18 -0400

    - Created home page (#3) - Installed Angular Material to project (#4)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+// As you add more providers to the app, they should be declared here.
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
 };
