33cc50f024e4963be1f81e9dccb3f3c9a79731c1
GetGoh <agohca@gmail.com>
2024-05-29 17:32:30 -0400

    Added zxing barcode scanner for testing. Needed to generate ssl certificate to get permission for phone camera. Default camera works on some barcodes and qr codes, but not all. Need to investigate. #5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { BarcodeFormat } from '@zxing/library';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [RouterOutlet, MatToolbarModule, ZXingScannerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'pricewise';
+  barcodeValue = 'Initial value';
+  cameraEnabled: boolean = true;
+  toggleCameraButtonText = 'Enable camera';
+
+  // this allows all formats from the BarcodeFormat enum
+  allowedFormats = [ BarcodeFormat.AZTEC, BarcodeFormat.QR_CODE, BarcodeFormat.CODABAR, BarcodeFormat.CODE_128, BarcodeFormat.CODE_39, BarcodeFormat.CODE_93, BarcodeFormat.DATA_MATRIX, BarcodeFormat.EAN_13, BarcodeFormat.EAN_8, BarcodeFormat.ITF, BarcodeFormat.MAXICODE, BarcodeFormat.PDF_417, BarcodeFormat.RSS_14, BarcodeFormat.RSS_EXPANDED, BarcodeFormat.UPC_A, BarcodeFormat.UPC_E, BarcodeFormat.UPC_EAN_EXTENSION ];
+
+  scanSuccessHandler(successString: string) {
+    this.barcodeValue = successString;
+  }
+
+  toggleCamera() {
+    this.cameraEnabled = this.cameraEnabled == false ? true : false;
+    this.toggleCameraButtonText = this.cameraEnabled == true ? 'Disable camera' : 'Enable camera';
+  }
+
 }
